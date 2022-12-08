127c9e4da2898c89c842b86310687724067a381e
Sarahfk <44497724+Sarahfk@users.noreply.github.com>
2022-12-08 20:13:44 +0100

    network pruning bugfix

--- BNReasoner.py
+++ BNReasoner.py
@@ -2,8 +2,6 @@ from typing import Union
 from BayesNet import BayesNet
 import pandas as pd
 import networkx as nx
-import matplotlib.pyplot as plt
-import copy as copy
 class BNReasoner:
     def __init__(self, net: Union[str, BayesNet]):
         """
@@ -30,7 +28,7 @@ class BNReasoner:
         return self.bn.get_all_variables()    
     
 ############################################################################################# Methods to implement
-    def Network_Pruning(self, query_var, e):
+    def Network_Pruning(self, query_vars, e):        
         #delete outgoing edges from nodes in e
         evidence_vars = e.index.values        
         for evidence in evidence_vars:            
@@ -39,22 +37,24 @@ class BNReasoner:
                 self.bn.del_edge((evidence, node))                         
                 #apply factor reduction (zeroing out the instances that are incompatible with e)
                 cpt = self.bn.get_cpt(node)
-                self.bn.reduce_factor(e, cpt)                                
-            #delete any leaf nodes that do not appear in Q or e (iteratively)
-            iteration = 0
-            while iteration == 0 or LeafNodesPresent >= 1:
-                iteration += 1
-                LeafNodesPresent = 0
-                vars = self.bn.get_all_variables()
+                new_cpt = self.bn.reduce_factor(e, cpt)
+                self.bn.update_cpt(node, new_cpt)
+                                               
+        #delete any leaf nodes that do not appear in Q or e (iteratively)
+        iteration = 0
+        while iteration == 0 or LeafNodesPresent >= 1:
+            iteration += 1
+            LeafNodesPresent = 0
+            vars = self.bn.get_all_variables()
+            for query_var in query_vars:
                 vars.remove(query_var)
-                vars.remove(evidence)
-                for var in vars:
-                    children = self.bn.get_children(var)
-                    if len(children) == 0:                        
-                        LeafNodesPresent += 1 
-                        self.bn.del_var(var)             
-        #alter CTP accordingly
-        self.Marginalization(query_var)#f?
+            for evidence_var in evidence_vars:
+                vars.remove(evidence_var)
+            for var in vars:
+                children = self.bn.get_children(var)
+                if len(children) == 0:                        
+                    LeafNodesPresent += 1 
+                    self.bn.del_var(var)
     
     def D_separated(self, x,y,z):
         vars = self.bn.get_all_variables()
@@ -112,7 +112,6 @@ class BNReasoner:
     
     def Maxing_Out(self, x):
         cpt = self.bn.get_cpt(x)
-        #print(cpt)
         old_length = int(len(cpt.index))
         row_names = cpt.index.values
         uneven_indexes = []
@@ -141,9 +140,9 @@ class BNReasoner:
         return h
         # return h (which is fg) 
            
-    def Ordering(self,set_var):
-        heuristic = 'min-fill'#heuristic is min-degree or min-fill
-        if heuristic == 'min-ordering':
+    def Ordering(self,set_var, heuristic):
+        #heuristic is min-degree or min-fill
+        if heuristic == 'min-degree':
             ordering = []
             length = len(set_var) 
             graph = self.bn.get_interaction_graph()
@@ -177,78 +176,8 @@ class BNReasoner:
                 ordering.append(set_var[0])                 
             return ordering  
         elif heuristic == 'min-fill':
-            ordering = []
-            originalgraph = copy.deepcopy(self.bn.get_interaction_graph())
-            originalset_var = set_var
-            nx.draw(originalgraph, with_labels=True, node_size=3000)
-            plt.show()
-            # cycle:
-            for i in range(len(originalset_var)):
-                neighbors = dict()
-                fill_value_dict = dict()
-                originaledges = originalgraph.edges()
-                for var in set_var:
-                    neighbors[var] = list(nx.neighbors(originalgraph, var))
-                #THIS INNER LOOP WORKS, SO ONLY 1 thing left to do: fix the while loop
-                for i in range(len(set_var)):
-                    graph = copy.deepcopy(originalgraph)
-                    edges = graph.edges()
-                    #i = 3
-                    fill_value = 0
-                    print('set_var[i] =', set_var[i])
-                    #1. Remove edges from/to to set_var[i]
-                    for neighbor in neighbors[set_var[i]]:
-                        try:
-                            graph.remove_edge(set_var[i], neighbor)
-                        except:
-                            graph.remove_edge(neighbor, set_var[i])
-                    #2. Remove set_var[i] from graph
-
-                    graph.remove_node(set_var[i])
-
-                    #3. try if there are still direct connections between the old neighbors of i
-                    for neighbor in range(len(neighbors[set_var[i]])-1):
-                        for neighbor2 in range(neighbor+1, len(neighbors[set_var[i]])):
-                            listforval = neighbors.get(set_var[i])
-                            value1 = listforval[neighbor]
-                            value2 = listforval[neighbor2]
-                            print('value1 = ', value1)
-                            print('value2 = ', value2)                    
-                            if graph.has_edge(value1, value2) == False:
-                                fill_value +=1  #if not a connection, it needs an additional edge, thus fillvalue+1
-                    fill_value_dict.update({set_var[i] :  fill_value})
-                    # add key of smallest value of dictionary to the elimination queue
-                eliminationkey = min(fill_value_dict, key = fill_value_dict.get)
-                print('fill_value_dict =' , fill_value_dict)
-                ordering.append(eliminationkey)
-                print('ordering = ', ordering)
-                
-                #deleting actual edges and nodes and adding edges that dont exist yet
-                for neighbor in neighbors[eliminationkey]:
-                    try:
-                        originalgraph.remove_edge(eliminationkey, neighbor)
-                    except:
-                        originalgraph.remove_edge(neighbor, eliminationkey)
-                
-                originalgraph.remove_node(eliminationkey)
-
-                for neighbor in range(len(neighbors[eliminationkey])-1):
-                    for neighbor2 in range(neighbor+1, len(neighbors[eliminationkey])):
-                        listforval = neighbors.get(eliminationkey)
-                        value1 = listforval[neighbor]
-                        value2 = listforval[neighbor2]
-                        if originalgraph.has_edge(value1, value2) == False:
-                            originalgraph.add_edge(value1, value2)
-                set_var.remove(eliminationkey)
-                #nx.draw(originalgraph, with_labels=True, node_size=3000)
-                #plt.show()
-                print('set_var =' , set_var)
-                print('edges = ', originaledges)
-            print(ordering)
-            return ordering
-
-
-
+            #implement second heuristic   
+            pass           
         # return good ordering for elimination of set_var based on min-degree heuristics and min-fill heuristics
     
     def Variable_Elimination(self, input_net, set_var):
@@ -264,43 +193,59 @@ class BNReasoner:
     def MAP(self, q, e):
         pass
     # return maximum a-posteriory instantion + value q
+    
     def MEP(self, e):
+        MPE = []
+        #first apply network pruning given evidence e
+        vars = self.bn.get_all_variables()
+        self.Network_Pruning(vars, e)
+        
+        #get elimination order from ordering?
+        ordering = self.Ordering(vars, "min-degree")
+        
+        #for each var: get max prob. and assign that truth value and update with this prob. factor 
+        for var in ordering:  
+            #get max cpt["p"]          
+            cpt = self.bn.get_cpt(var)
+            print(cpt)
+            
+            #set this truth value and append to MPE
+            #for all cpt's containing var: apply factor multiplication of max * current p
+                                
         pass
-    # return most probable explanation given e
+        # return most probable explanation given e
 
 class main():
     # Variables
     Truth_value = True
     Var = "dog-out"
     Evidence = pd.Series({Var : Truth_value})
-    Query_var = "family-out"
+    Query_var = ["family-out"]
     x = ['bowel-problem', 'dog-out']
     y = ['hear-bark']
     z = ['family-out']
     
     #Init net
-    # NET = BNReasoner("testing/lecture_example.BIFXML") #initializing network)
-    NET = BNReasoner("testing/lecture_example.BIFXML") #initializing network)   
+    NET = BNReasoner("testing/dog_problem.BIFXML") #initializing network)
+    # NET = BNReasoner("testing/lecture_example.BIFXML") #initializing network)   
         
-    #show NET --> works
-    #NET.Draw()      
+    #show NET 
+    # NET.Draw()      
+    
     #Applying network pruning
-    # NET.Network_Pruning(Query_var, Evidence)
+    NET.Network_Pruning(Query_var, Evidence)
     
     #Applying marginalization    
-    # Var2 = 'hear-bark'
-    # cpt = NET.Get_CPT(Var2)
-    # f = cpt['p']   
-    # NET.Marginalization(f, Var2)  
-    #Var2 = 'hear-bark'
-    #cpt = NET.Get_CPT(Var2)
-    #f = cpt['p']   
-    #NET.Marginalization(f, Var2)  
-
-    #test for ordering
+    Var2 = 'hear-bark'
+    cpt = NET.Get_CPT(Var2)
+    f = cpt['p']   
+    # NET.Marginalization(f, Var2)
+    
+    #Applying MPE
+    # NET.MEP(Evidence)      
     
     #finding a good ordering for variable elimination
-    NET.Ordering(NET.Get_Vars())
+    # NET.Ordering(NET.Get_Vars())
     
 if __name__ == "__main__":
     main()
